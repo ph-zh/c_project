9bc1e173e0811c836aa91f70b20e07f69f2016dd
phzh <p.zhavoronkov@gmail.com>
2026-06-02 16:25:11 +0200

    math functions

--- main.c
+++ main.c
@@ -1,44 +1,22 @@
 #include <stdio.h>
-#include <string.h>
+#include <math.h>
 
 int main() {
 
-    char noun[50] = "";
-    char verb[50] = "";
-    char adjective1[50] = "";
-    char adjective2[50] = "";
-    char adjective3[50] = "";
+    float x = 45;
 
-    printf("Enter an adjective (description): ");
-    fgets(adjective1, sizeof(adjective1), stdin);
-    adjective1[strlen(adjective1) - 1] = '\0';
+    //x = sqrt(x);
+    //x = pow(x, 4);
+    //x = round(x);
+    //x = ceil(x);
+    //x = floor(x);
+    //x = abs(x);
+    //x = log(x);
+    //x = sin(x);
+    //x = cos(x);
+    x = tan(x);
 
-    printf("Enter a noun (animal or person): ");
-    fgets(noun, sizeof(noun), stdin);
-    noun[strlen(noun) - 1] = '\0';
-
-    printf("Enter an adjective (description): ");
-    fgets(adjective2, sizeof(adjective2), stdin);
-    adjective2[strlen(adjective2) - 1] = '\0';
-
-    printf("Enter a verb (ending w/ -ing): ");
-    fgets(verb, sizeof(verb), stdin);
-    verb[strlen(verb) - 1] = '\0';
-
-    printf("Enter an adjective (description): ");
-    fgets(adjective3, sizeof(adjective3), stdin);
-    adjective3[strlen(adjective3) - 1] = '\0';
-
-    // printf("%s\n", noun);
-    // printf("%s\n", verb);
-    // printf("%s\n", adjective1);
-    // printf("%s\n", adjective2);
-    // printf("%s\n", adjective3);
-
-    printf("\nToday i went to a %s zoo.\n", adjective1);
-    printf("In an exhibit, I saw a %s.\n", noun);
-    printf("%s was %s and %s!\n", noun, adjective2, verb);
-    printf("I was %s!\n", adjective3);
+    printf("%f", x);
 
     return 0;
 }
