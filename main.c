c9996da5eb51fded4f8db74f1cc0b0e49148bce4
phzh <p.zhavoronkov@gmail.com>
2026-06-03 08:54:55 +0200

    circle calculator program

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include <math.h>
+#include <windows.h>
 
 int main() {
 
-    float x = 45;
+    SetConsoleCP(CP_UTF8);
 
-    //x = sqrt(x);
-    //x = pow(x, 4);
-    //x = round(x);
-    //x = ceil(x);
-    //x = floor(x);
-    //x = abs(x);
-    //x = log(x);
-    //x = sin(x);
-    //x = cos(x);
-    x = tan(x);
+    double radius = 0.0;
+    double area = 0.0;
+    double surfaceArea = 0.0;
+    double volume = 0.0;
+    const double PI = 3.14159;
 
-    printf("%f", x);
+    printf("Enter the radius: ");
+    scanf("%lf", &radius);
+
+    area = PI * pow(radius, 2);
+    surfaceArea = 4 * PI * pow(radius,2);
+    volume = (4.0 / 3.0) * PI * pow(radius, 3);
+
+    printf("Area: %.2lf cm³\n", area);
+    printf("Surface cm³ area: %.2lf cm³\n", surfaceArea);
+    printf("Volume area: %.2lf cm³\n", volume);
 
     return 0;
 }
