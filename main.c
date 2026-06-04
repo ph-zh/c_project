6e4b820efef0b980370ab0c444ea8bea75968357
phzh <p.zhavoronkov@gmail.com>
2026-06-04 11:17:08 +0200

    if else task example

--- main.c
+++ main.c
@@ -1,32 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
 
-    double principal = 0.0;
-    double rate = 0.0;
-    int years = 0;
-    int timesCompounded = 0;
-    double total = 0.0;
+    int age = 0;
 
-    printf("Compound Interest Calculator\n");
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
-    printf("Enter the principal (P): ");
-    scanf("%lf", &principal);
-
-    printf("Enter the interest rate %% (r): ");
-    scanf("%lf", &rate);
-    rate = rate / 100;
-
-    printf("Enter the # of years (t): ");
-    scanf("%d", &years);
-
-    printf("Enter # of times compunded per year (n): ");
-    scanf("%d", &timesCompounded);
-
-    total = principal * pow(1 + rate / timesCompounded, timesCompounded * years);
-
-    printf("After %d years, the total will be $%.2lf", years, total);
+    if(age >= 65) {
+        printf("You are a senior\n");
+    } else if(age > 18) {
+        printf("You are an adult\n");
+    } else if(age < 0) {
+        printf("You haven't born yet\n");
+    } else if (age == 0) {
+        printf("You are a newborn\n");
+    } else {
+        printf("You are a child\n");
+    }
 
     return 0;
 }
