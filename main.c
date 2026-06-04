366c079a5bb0693f01dffe8bf4c99798c0f86685
phzh <p.zhavoronkov@gmail.com>
2026-06-04 11:37:29 +0200

    if else task with user input

--- main.c
+++ main.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <string.h>
 
 int main() {
 
-    bool isStudent = false;
-    
-    if(isStudent) {
-        printf("You are a student");
+    char name[50] = "";
+
+    printf("Enter your name: ");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) - 1] = '\0';
+
+    if(strlen(name) == 0) {
+        printf("You did not enter your name");
     } else {
-        printf("You are not a student");
+        printf("Hello %s", name);
     }
 
     return 0;
