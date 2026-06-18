0394de631bbdc83a10576a528f4edf5cbffc77e6
phzh <p.zhavoronkov@gmail.com>
2026-06-18 11:23:42 +0200

    switch operator

--- main.c
+++ main.c
@@ -2,28 +2,41 @@
 
 int main() {
 
-    char choise = '\0';
-    float fahrenheit = 0.0f;
-    float celsius = 0.0f;
+    char dayOfWeek = '\0';
+    printf("Enter a day of the week (M, T, W, R, F, S, U): ");
+    scanf("%c", &dayOfWeek);
 
-    printf("Temperature conversion program\n");
-    printf("C. Celcius to Fahrenheit\n");
-    printf("F. Fahrenheit to Celcius\n");
-    printf("Is the temp is in Celcius(C) or Fahrenheit(F): ");
-    scanf("%c", &choise);
+    switch(dayOfWeek) {
+        case 'M':
+            printf("It is Monday");
+            break;
 
-    if(choise == 'C') {
-        printf("Enter the temperature in Celcius: ");
-        scanf("%f", &celsius);
-        fahrenheit = (celsius * 9 / 5) + 32;
-        printf("%.1f Celcius is equal to %.1f Fahrenheit\n", celsius, fahrenheit);
-    } else if (choise == 'F') {
-        printf("Enter the temperature in Fahrenheit: ");
-        scanf("%f", &fahrenheit);
-        celsius = (fahrenheit - 32) * 5 / 9;
-        printf("%.1f Fahrenheit is equal to %.1f Celcius\n", fahrenheit, celsius);
-    } else {
-        printf("Invalid choise! Please select C or F\n");
+        case 'T':
+            printf("It is Tuesday");
+            break;
+
+        case 'W':
+            printf("It is Wednesday");
+            break;
+        
+        case 'R':
+            printf("It is Thursday");
+            break;
+        
+        case 'F':
+            printf("It if Friday");
+            break;
+        
+        case 'S':
+            printf("It is Saturday");
+            break;
+        
+        case 'U':
+            printf("It is Sunday");
+            break;
+
+        default:
+            printf("Please only enter a character (M, T, W, R, F, S, U)");
     }
 
     return 0;
