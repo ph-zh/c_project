cc0963188544119c9b3f7e2b6e512960cfcf5db8
phzh <p.zhavoronkov@gmail.com>
2026-06-25 15:53:21 +0200

    variable scope

--- main.c
+++ main.c
@@ -1,60 +1,26 @@
 #include <stdio.h>
-#include <stdbool.h>
 
-int getMax(int x, int y) {
-    if(x >= y) {
-        return x;
-    } else{
-        return y;
-    }
+int add(int x, int y) {
+    int result = x + y;
+    return result;
 }
 
-bool ageCheck(int age) {
-    
-    if(age >= 18) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-double cube(double num) {
-    return num * num * num;
-}
-
-double square(double num) {
-    return num * num;
+int subtract(int x, int y) {
+    int result = x - y;
+    return result;
 }
 
 int main() {
 
-    double x = square(2.1);
-    double y = square(3.2);
-    double z = square(4.3);
 
-    double a = cube(2);
-    double b = cube(3);
-    double c = cube(4);
+    int x = 5;
+    int y = 6;
 
-    int age = 21;
+    int result = subtract(x,y);
+    
+    printf("%d", result);
 
-    int max = getMax(5,3);
 
-    printf("%lf\n", x);
-    printf("%lf\n", y);
-    printf("%lf\n", z);
-
-    printf("%lf\n", a);
-    printf("%lf\n", b);
-    printf("%lf\n", c);
-
-    if(ageCheck(age)) {
-        printf("You may sign up\n");
-    } else {
-        printf("You must be 18+ to sign up\n");
-    }
-
-    printf("%d\n", max);
 
     return 0;
 }
