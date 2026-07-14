5e7eab8a0a0696cca42160e91aea1d36362a101b
phzh <p.zhavoronkov@gmail.com>
2026-07-14 10:59:10 +0200

    yard to kilometer

--- main.c
+++ main.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
-#include <stdbool.h>
-
-void hello(char name[], int age); // function prototype
-bool ageCheck(int age);
 
 int main() {
 
-    hello("FreankSpignetti", 30);
+    int miles, yards;
+    float kilometers;
 
-    if(ageCheck(14)) {
-        printf("You are old enough to work at pizza bar");
-    } else {
-        printf("You must be 16+ to work at pizza bar");
-    }
+    miles = 26;
+    yards = 385;
+    kilometers = 1.609 * (miles + yards / 1760.0);
 
-   
+    printf("\nA marathon is %f kilometers. \n\n", kilometers);
     return 0;
-}
-
-void hello(char name[], int age) {
-    printf("Hello %s\n", name);
-    printf("You are %d years old\n", age);
-}
-
-bool ageCheck(int age) {
-    if(age >= 16) {
-        return true;
-    } 
-    return false;
 }
