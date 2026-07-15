19b2cf5792de3998d75f3b39e81b8b343d1e180d
phzh <p.zhavoronkov@gmail.com>
2026-07-15 15:10:31 +0200

    while loop

--- main.c
+++ main.c
@@ -1,14 +1,38 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
-int main() {
+int main(void) {
 
-    int miles, yards;
-    float kilometers;
+    // char name[50] = "";
 
-    miles = 26;
-    yards = 385;
-    kilometers = 1.609 * (miles + yards / 1760.0);
+    // printf("Enter your name: ");
+    // fgets(name, sizeof(name), stdin);
+    // name[strlen(name) - 1] = '\0';
 
-    printf("\nA marathon is %f kilometers. \n\n", kilometers);
+    // while(strlen(name) == 0) { // lenght of the string
+    //     printf("Name cannot be empty! Please enter your name: ");
+    //     fgets(name, sizeof(name), stdin);
+    //     name[strlen(name) - 1] = '\0';
+
+    // }
+
+    // printf("Hello %s\n", name);
+    // printf("Number of letters in your name is: %d", strlen(name));
+
+    bool isRunning = true;
+    char response = '\0';
+
+    do {
+        printf("You are playing a game\n");
+        printf("Whould you like to continue ? (Y = yes, N = no): ");
+        scanf(" %c", &response);
+
+        if(response != 'Y' && response != 'y') {
+            isRunning = false;
+        }
+    } while(isRunning);
+    printf("You exit the game");
+
     return 0;
 }
